c6a2c7883992926c6cbded8e68a9ae415cca48d0
jakublala <jakublala@gmail.com>
2022-05-28 21:35:16 +0100

    TRIAtomic molecule

--- triatomic.py
+++ triatomic.py
@@ -121,96 +121,103 @@ class ODEFunc_NoEndFixed():
         return state
 
 
-    # def integrate_verlet_explicit(self, state, dt):
+    def integrate_verlet_explicit(self, state, dt):
+        v,q = copy.deepcopy(state)
+        assert q[1] > q[0], 'the two atoms collided'
         
-    #     def fce_force(q):
-    #         q1 = q[0]
-    #         q2 = q[1]
+        force = self.separate_force_explicit(q, 0.001)
+        self.forces_log.append(force.cpu().numpy().squeeze())
+        
+        dvdt_0 = force / self.mass 
+        
+        v_step_half = 1/2 * dvdt_0 * dt 
+        v = v + v_step_half
+        
+        # explicitly defined change in q
+        q_step_full = v * dt
+        q = q + q_step_full
+        
+        # gradient full at t + dt 
+        force = self.separate_force_explicit(q, 0.001)
+        
+        dvdt_full = force / self.mass 
+        
+        v_step_full = v_step_half + 1/2 * dvdt_full * dt
+        
+        new_v = state[0] + v_step_full
+        new_q = state[1] + q_step_full
+        
+        state = (new_v.detach(), new_q.detach())
+        
+        return state
 
-    #         x_A = q1 - 0
-    #         x_B = q2 - q1
-
-    #         force1 = -k*(torch.abs(x_A) - r0) * torch.sign(x_A) + k * (torch.abs(x_B) - r0) * torch.sign(x_B)
-    #         force2 = - k * (torch.abs(x_B) - r0) * torch.sign(x_B)
-
-    #         return torch.Tensor([force1, force2]).view(-1, 1).to(device)
-
-    #     v,q = copy.deepcopy(state)
-    #     assert q[1] > q[0], 'the two atoms collided'
-        
-    #     force = fce_force(q)
-    #     # self.forces_log.append(force.cpu().numpy().squeeze())
-        
-    #     dvdt_0 = force / self.mass 
-        
-    #     v_step_half = 1/2 * dvdt_0 * dt 
-    #     v = v + v_step_half
-        
-    #     # explicitly defined change in q
-    #     q_step_full = v * dt
-    #     q = q + q_step_full
-        
-    #     # gradient full at t + dt 
-    #     force = fce_force(q)
-        
-    #     dvdt_full = force / self.mass 
-        
-    #     v_step_full = v_step_half + 1/2 * dvdt_full * dt
-        
-    #     new_v = state[0] + v_step_full
-    #     new_q = state[1] + q_step_full
-        
-    #     state = (new_v.detach(), new_q.detach())
-        
-    #     return state
+    def separate_force_explicit(self, q, dq):
+        # HACK
+        k = 1.0
+        qs = ([torch.abs(q[1] - q[0]) - self.r0, torch.abs(q[2] - q[1]) - self.r0])
+        # grad_A = (self.pair_potential_explicit(qs[0]+dq) - self.pair_potential_explicit(qs[0]-dq)) / (2 * dq)
+        # grad_B = (self.pair_potential_explicit(qs[1]+dq) - self.pair_potential_explicit(qs[1]-dq)) / (2 * dq)
+        grad_A = k * qs[0]
+        grad_B = k * qs[1]
+        grad = torch.Tensor([grad_A, -grad_A + grad_B, -grad_B]).view(-1, 1).to(device)
+        return grad
+    
 num_models = 5
-
 dt = 0.1 # how is dt defined?
 
-traj_lengths = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
-num_samples = 50
-data = []
-start = time.perf_counter()
-for l in traj_lengths:
-    traj_length_data = []
-    for m in range(1, num_models+1):
-        func = torch.load(f'results/spring/{m}_model.pt').to(device)
-        func_system = ODEFunc_NoEndFixed(func)
-        
-        states_data = []
-        for n in range(num_samples):
-            
-            # axis = 0 => bond id; axis = 1 => (momentum, position)
-            # get a random number from 0 to 1
+# max_value = 1000
+# traj_lengths = [10*i for i in range(1, 1+(1000 // 10))]
 
-            # position0 = 0.0
-            # position1 = 1.0
-            # position2 = 1.7
-            # velocity0 = 0.0
-            # velocity1 = 0.0
-            # velocity2 = 0.0
+num_samples = 1
+# data = dict()
+# for t in traj_lengths:
+#     data[t] = torch.zeros(3, 2).to(device)
 
-            # state = torch.Tensor([[velocity0, position0], [velocity1, position1], [velocity2, position2]]).view(3, -1).to(device)
-            state = torch.rand((3, 2)).to(device)
-            state[:, 0] = 0
-            state[0, 1] -= 1
-            state[-1, 1] =+ 1
+data_model = []
+data_explicit = []
 
-            states = [state.detach().cpu().numpy()]
-            explicit_states = [state.detach().cpu().numpy()]
+for m in range(1, num_models+1):
+    func = torch.load(f'results/spring/{m}_model.pt').to(device)
+    func_system = ODEFunc_NoEndFixed(func)
+    
+    for n in range(num_samples):
+        state = torch.rand((3, 2)).to(device)
+        state[:, 0] = 0
+        state[0, 1] -= 1
+        state[-1, 1] =+ 1
 
-            state = torch.split(state, 1, dim=1)
-            explicit_state = copy.deepcopy(state)
+        states = [state.detach().cpu().numpy()]
+        explicit_states = [state.detach().cpu().numpy()]
 
+        state = torch.split(state, 1, dim=1)
+        explicit_state = copy.deepcopy(state)
 
-            with torch.no_grad():
-                for i in range(l):
-                    state = func_system.integrate_verlet(state, dt)
-                    new_state = torch.cat(state, dim=1).detach().cpu().numpy().reshape(3, -1)
-                    states.append(new_state)
-                    
-            states_data.append(np.array(states))
-        
-        traj_length_data.append(states_data)
-    traj_length_data = np.array(traj_length_data)
-    np.save(f'results/spring/divergence/{l}_traj_length_data.npy', traj_length_data)
+        with torch.no_grad():
+            for i in range(1, 10000+1):
+                state = func_system.integrate_verlet(state, dt)
+                new_state = torch.cat(state, dim=1).detach().cpu().numpy().reshape(3, -1)
+                states.append(new_state)
+
+                explicit_state = func_system.integrate_verlet_explicit(explicit_state, dt)
+                new_explicit_state = torch.cat(explicit_state, dim=1).detach().cpu().numpy().reshape(3, -1)
+                explicit_states.append(new_explicit_state)
+
+                # if i in traj_lengths:
+                #     plt.plot(np.array(states)[:, :, 0], 'r', label='momentum model')
+                #     plt.plot(np.array(explicit_states)[:, :, 0], 'k--', label='momentum explicit')
+                #     plt.xlim(len(states)-10, len(states))
+                #     plt.savefig(f'temp/{i}.png')
+                #     plt.close()
+
+                #     mom_loss = torch.abs(torch.abs(state[0] - explicit_state[0]) / explicit_state[0]).to(device)
+                #     pos_loss = torch.abs(torch.abs(state[1] - explicit_state[1]) / explicit_state[1]).to(device)
+                #     data[i] += torch.cat([mom_loss, pos_loss], dim=1)
+
+        data_model.append(np.array(states))
+        data_explicit.append(np.array(explicit_states))
+
+data_model = np.array(data_model)
+data_explicit = np.array(data_explicit)
+
+np.save(f'results/spring/data_model.npy', data_model)
+np.save(f'results/spring/data_explicit.npy', data_explicit)
